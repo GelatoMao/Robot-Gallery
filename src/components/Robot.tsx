506f883f075989b481f8dd71d62e5cfb8258a91f
lumao <lumao@ebay.com>
2021-08-16 00:46:28 +0800

    Use usecontext

--- src/components/Robot.tsx
+++ src/components/Robot.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { appContext } from "../index";
 import styles from "./Robot.module.css";
 
@@ -10,19 +10,14 @@ interface RobotProps {
 
 // 对象可以直接使用花括号进行展开
 const Robot: React.FC<RobotProps> = ({ id, name, email }) => {
+  const value = useContext(appContext);
   return (
-    <appContext.Consumer>
-      {(value) => {
-        return (
-          <div className={styles.cardContainer}>
-            <img alt="robot" src={`https://robohash.org/${id}`} />
-            <h2>{name}</h2>
-            <p>{email}</p>
-            <p>作者：{value.username}</p>
-          </div>
-        );
-      }}
-    </appContext.Consumer>
+    <div className={styles.cardContainer}>
+      <img alt="robot" src={`https://robohash.org/${id}`} />
+      <h2>{name}</h2>
+      <p>{email}</p>
+      <p>作者：{value.username}</p>
+    </div>
   );
 };
 
